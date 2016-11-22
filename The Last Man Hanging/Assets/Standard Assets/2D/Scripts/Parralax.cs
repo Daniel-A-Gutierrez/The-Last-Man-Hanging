d43eddef3da120a9f464c7a43a66ff0b1ea0fd1a
Daniel Gutierrez <icanmakespaghetti@gmail.com>
2016-11-22 15:41:18 -0800

    Parralax and Music
    Both work now, there is frame stuttering .

--- The Last Man Hanging/Assets/Standard Assets/2D/Scripts/Parralax.cs
+++ The Last Man Hanging/Assets/Standard Assets/2D/Scripts/Parralax.cs
@@ -1,5 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+/*so i need a few numbers . i need the width of the layer in pixels and in gamespace, i need the leftmost 
+ coordinate of it and the left and rightmost coordinates of the camera view, and i need the coordinates of the
+ leftmost part of the layer and 1/3 way through the layer in gamespace. once i have these things this is what ill do
+ set all left borders equal to the left camera border. record the initial position of the transform of the layer
+ at this position. once the camera's right border has reached the 2/3 or full point of the image, reset it to 
+ that initial x position*/
+
+
+
+
 
 public class Parralax : MonoBehaviour {
 
@@ -46,22 +56,34 @@ public class Parralax : MonoBehaviour {
         layers = GameObject.FindGameObjectsWithTag("BackgroundLayer");
         foreach (GameObject go in layers)
         {
-            
-                
                 initialx[I] = transform.GetChild(I).localPosition.x;
-                I++;
-            
-            
+                I++;          
         }
+        I--;
+        //print("Object name " + layers[I].gameObject.ToString());
+        //print("sprite is null " + layers[I].GetComponent<SpriteRenderer>().sprite == null);
+        //print("pixel width output " + layers[I].GetComponent<SpriteRenderer>().sprite.rect.width/ layers[I].GetComponent<SpriteRenderer>().sprite.pixelsPerUnit);
+        //print("world width output " + layers[I].GetComponent<BoxCollider2D>().size * layers[I].transform.localScale.x);
+        //print("Rect output " + layers[I].GetComponent<SpriteRenderer>().sprite.rect);
+        //print("Rect output " + layers[I].GetComponent<SpriteRenderer>().sprite.rect);
+        //print("Pixels per unity output " + layers[I].GetComponent<SpriteRenderer>().sprite.pixelsPerUnit);
+        //print("Camera rect " + GameObject.FindWithTag("MainCamera").GetComponent<Camera>().rect);
+        //print("Camera ortho size " + GameObject.FindWithTag("MainCamera").GetComponent<Camera>().orthographicSize);
+        //print("Camera aspect ratio " + GameObject.FindWithTag("MainCamera").GetComponent<Camera>().aspect);
+        //print("Camera true borders in world space " + -GameObject.FindWithTag("MainCamera").GetComponent<Camera>().orthographicSize *
+        //    GameObject.FindWithTag("MainCamera").GetComponent<Camera>().rect.width/2 );
+
+
         I = 0;
         foreach (GameObject go in layers)
         {
-            print(go.GetComponent<BoxCollider2D>().size.x);
+            //print(go.GetComponent<BoxCollider2D>().size.x);
 
-            widths[I] =  go.transform.position.x;
-            sets[I] = widths[I];
+            widths[I] =  go.GetComponent<BoxCollider2D>().size.x * go.transform.localScale.x;// THIS CAN CAUSE PROBLEMS
+            sets[I] = widths[I] *1/3;
             I++;
         }
+        I--;
     }
 	
 	// Update is called once per frame
@@ -77,7 +99,6 @@ public class Parralax : MonoBehaviour {
                 speeds[i] * mc.GetComponent<CameraScroll>().speed * Time.deltaTime, layers[i].transform.localPosition.y);
             if(initialx[i] - layers[i].transform.localPosition.x  >= sets[i])
             {
-                print("asdfasdfasdf");
                 layers[i].transform.localPosition = new Vector2(initialx[i],0);
             }
         }
