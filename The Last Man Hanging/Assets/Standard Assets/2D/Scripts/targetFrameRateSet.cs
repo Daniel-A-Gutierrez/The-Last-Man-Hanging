42b8487bc0971aaf67ff52981b179d1f28c560fc
Daniel Gutierrez <icanmakespaghetti@gmail.com>
2016-12-16 00:47:34 -0800

    bye bye fps cap

---
+++ The Last Man Hanging/Assets/Standard Assets/2D/Scripts/targetFrameRateSet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class targetFrameRateSet : MonoBehaviour {
+
+    // Use this for initialization
+    public int targetFrameRate;
+	void Start () {
+	
+	}
+	void Awake()
+    {
+        Application.targetFrameRate = targetFrameRate;
+    }
+	// Update is called once per frame
+	void Update () {
+        print(1 / Time.deltaTime);
+	}
+}
